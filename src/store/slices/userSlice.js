8c5185c4ac7cf1818f70348908114093179a8380
pastendi <109973861+pastendi@users.noreply.github.com>
2023-08-11 09:38:52 +0545

    addition of fetchUser function

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,4 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+  const res = await axios.get('http://localhost:5000/users')
+  return res.data
+})
 
 const userSlice = createSlice({
   name: 'user',
@@ -7,7 +12,23 @@ const userSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchUsers.pending, (state, action) => {
+        state.isLoading = true
+        state.error = null
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.data = action.payload
+        state.error = null
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+  },
 })
 
 export const userReducer = userSlice.reducer
+export { fetchUsers }
